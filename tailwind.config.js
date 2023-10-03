3e79c6d7ed7e26a707bb2e54e78c1e419dc10c2f
Purna Shrestha <crush9918@gmail.com>
2023-10-03 11:05:10 +0545

    Declare the necessary colors for future reference

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,18 @@
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "bg-color": "#021a24",
+        "root": "#00cb5b",
+        "root-yellow": "#ffb001",
+        "light-green": "#1b5045",
+        "blue": "#0049df",
+        "red": "#f14d2a",
+        "white": "#E1F7EB",
+      },
+    },
   },
   plugins: [],
   darkMode: "class",
-}
+};
