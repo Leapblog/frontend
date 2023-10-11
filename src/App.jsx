e02d62980818201bc877bd1e21398e0085863e64
Purna Shrestha <crush9918@gmail.com>
2023-10-11 21:11:48 +0545

    Create a bottom navbar for mobile devices issue #9

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./index.css";
 import Header from "./components/Header";
+import NavbarMobile from "./components/NavbarMobile";
 
 function App() {
   return (
     <>
       <Header />
+      <NavbarMobile />
     </>
   );
 }
